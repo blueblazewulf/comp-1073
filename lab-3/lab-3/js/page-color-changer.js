86f7925df288a4f5fd12170db5ff9061b54a165a
blueblazewulf <hunter.2g7@gmail.com>
2025-06-17 13:00:00 -0400

    ensure the page loads correctly and initialize the slider bars

---
+++ lab-3/lab-3/js/page-color-changer.js
@@ -0,0 +1,9 @@
+// Waits until the page has fully loaded before running the JS
+document.addEventListener('DOMContentLoaded', function () {
+    // Gets slider elements
+    const redSlider = document.getElementById('red');
+    const greenSlider = document.getElementById('green');
+    const blueSlider = document.getElementById('blue');
+    const htmlElement = document.querySelector('html');
+
+   
